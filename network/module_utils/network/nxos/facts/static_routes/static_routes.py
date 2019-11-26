c28f73bd2c2d22c14a1e4693de05e510f128f3d2
adharshsrivatsr <adharshsrivats@gmail.com>
2019-11-26 09:27:22 -0500

    Clean facts code

--- network/module_utils/network/nxos/facts/static_routes/static_routes.py
+++ network/module_utils/network/nxos/facts/static_routes/static_routes.py
@@ -10,7 +10,6 @@ for a given resource, parsed, and the facts tree is populated
 based on the configuration.
 """
 import re
-import q
 from copy import deepcopy
 from ansible.module_utils.network.common import utils
 from ansible.module_utils.network.nxos.argspec.static_routes.static_routes import Static_routesArgs
@@ -44,8 +43,6 @@ class Static_routesFacts(object):
         """
         vrf_data = []
         non_vrf_data = []
-        q(connection)
-        q(data)
         if not data:
             non_vrf_data = connection.get(
                 "show running-config | include '^ip(v6)* route'")
@@ -53,19 +50,16 @@ class Static_routesFacts(object):
                 "show running-config | section '^vrf context'")
             non_vrf_data = non_vrf_data.split('\n')
             vrf_data = vrf_data.split('\nvrf context')
-            q(vrf_data)
+            # as we split based on 'vrf context', it is stripped from the data except the first element
         else:
+            # used for parsed state where data is from the 'running-config' key
             data = data.split('\n')
-            q(data)
             i = 0
             while i <= (len(data)-1):
-                # q(i)
                 if 'vrf context ' in data[i]:
                     vrf_conf = data[i]
                     j = i+1
-                    # q(j)
                     while j < len(data) and 'vrf context ' not in data[j]:
-                        # q(data[j])
                         vrf_conf += '\n'+data[j]
                         j += 1
                     i = j
@@ -73,41 +67,28 @@ class Static_routesFacts(object):
                 else:
                     non_vrf_data.append(data[i])
                     i += 1
-        q(data, vrf_data, non_vrf_data)
         objs = []
-        # as we split based on 'vrf context', it is stripped from the data except the first element
         new_vrf_data = []
-        # q(vrf_data)
         for v in vrf_data:
-            if re.search('management', v):
-                continue
-                # ignore management vrf
             if re.search('\n\s*ip(v6)? route', v):
-                # q('hereh')
-                # q(v)
                 new_vrf_data.append(v)
                 # dont consider vrf if it does not have routes
-        q(new_vrf_data)
         for i in range(len(new_vrf_data)):
             if not re.search('^vrf context', new_vrf_data[i]):
                 new_vrf_data[i] = 'vrf context'+new_vrf_data[i]
 
         resources = non_vrf_data+new_vrf_data
-        q(resources)
         objs = self.render_config(self.generated_spec, resources)
 
         ansible_facts['ansible_network_resources'].pop('static_routes', None)
         facts = {}
-        # q(objs)
         if objs:
             params = utils.validate_config(
                 self.argument_spec, {'config': objs})
             params = utils.remove_empties(params)
-            # q(params)
             for c in params['config']:
                 if c == {'vrf': '__global__'}:
                     params['config'].remove(c)
-            # q(params)
             facts['static_routes'] = params['config']
         ansible_facts['ansible_network_resources'].update(facts)
         return ansible_facts
@@ -116,12 +97,9 @@ class Static_routesFacts(object):
         ''' 
         This method parses the command to create the innermost dictionary of the config
         '''
-
         conf = re.sub('\s*ip(v6)? route', '', conf)
         # strip 'ip route'
-        # q(conf)
         inner_dict['dest'] = re.match("^\s*(\S+\/\d+) .*", conf).group(1)
-
         # ethernet1/2/23
         iface = re.match(".* ([a-zA-Z0-9]+\d*\/\d+(\/?\.?\d*)*) .*", conf)
         if iface:
@@ -150,40 +128,33 @@ class Static_routesFacts(object):
         for key in keywords:
             pattern = re.match('.* (?:%s) (\S+).*' % key, conf)
             if pattern:
+                q(key)
+                if key == 'vrf':
+                    key = 'dest_vrf'
+                elif key == 'name':
+                    key = 'route_name'
                 inner_dict[key] = pattern.group(1).strip()
-                # q(key + ' ' + inner_dict[key])
                 conf = re.sub(key + ' ' + inner_dict[key], '', conf)
 
-        # q(conf)
         pref = re.match('(?:.*) (\d+)$', conf)
         if pref:
-            # q(pref)
+            # if something is left at the end without any key, it is the pref
             inner_dict['admin_distance'] = pref.group(1)
-
         return inner_dict
 
-    def process_command(self, conf, afi_list, dest_list, af):
-        '''
-        This uses the 
-        '''
+    def get_command(self, conf, afi_list, dest_list, af):
         inner_dict = {}
-
         inner_dict = self.get_inner_dict(conf, inner_dict)
         if inner_dict['afi'] not in afi_list:
             af.append({'afi': inner_dict['afi'], 'routes': []})
             afi_list.append(inner_dict['afi'])
+
         next_hop = {}
-        # q(inner_dict.keys())
         params = ['forward_router_address', 'interface',
-                  'admin_distance', 'name', 'tag', 'track', 'vrf']
+                  'admin_distance', 'route_name', 'tag', 'track', 'dest_vrf']
         for p in params:
             if p in inner_dict.keys():
-                if p == 'name':
-                    next_hop.update({'route_name': inner_dict[p]})
-                elif p == 'vrf':
-                    next_hop.update({'dest_vrf': inner_dict[p]})
-                else:
-                    next_hop.update({p: inner_dict[p]})
+                next_hop.update({p: inner_dict[p]})
 
         if inner_dict['dest'] not in dest_list:
             dest_list.append(inner_dict['dest'])
@@ -194,7 +165,6 @@ class Static_routesFacts(object):
         else:
             af[-1]['routes'][-1]['next_hops'].append(next_hop)
             # just append if dest already exists
-        # q(af)
         return af
 
     def render_config(self, spec, con):
@@ -214,32 +184,25 @@ class Static_routesFacts(object):
         global_dest_list = []
         if con:
             for conf in con:
-                # q(conf)
                 if conf.startswith('vrf context'):
                     svrf = re.match('vrf context (\S+)\n', conf).group(1)
-                    # q(svrf)
-                    if 'management' not in svrf:
-                        afi_list = []
-                        af = []
-                        dest_list = []
-                        config_dict = {'vrf': svrf, 'address_families': []}
-                        conf = conf.split('\n')
-                        # considering from the second line as first line is 'vrf context..'
-                        conf = conf[1:]
-                        for c in conf:
-                            if 'ip route' in c or 'ipv6 route' in c:
-                                self.process_command(
-                                    c, afi_list, dest_list, af)
-                                config_dict['address_families'] = af
-                    else:
-                        continue
+                    afi_list = []
+                    af = []
+                    dest_list = []
+                    config_dict = {'vrf': svrf, 'address_families': []}
+                    conf = conf.split('\n')
+                    # considering from the second line as first line is 'vrf context..'
+                    conf = conf[1:]
+                    for c in conf:
+                        if 'ip route' in c or 'ipv6 route' in c:
+                            self.get_command(
+                                c, afi_list, dest_list, af)
+                            config_dict['address_families'] = af
                     config.append(config_dict)
                 else:
                     if 'ip route' in conf or 'ipv6 route' in conf:
-                        self.process_command(
+                        self.get_command(
                             conf, global_afi_list, global_dest_list, global_af)
-                        # q(global_af)
             config.append(utils.remove_empties(
                 {'address_families': global_af}))
-            # q(config)
         return config
